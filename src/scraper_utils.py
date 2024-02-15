41061148c935d8c205588ae22bf8b3ca268ffcb4
sandrazec <112199747+sandrazec@users.noreply.github.com>
2024-02-14 21:53:12 -0500

    Update scraper_utils.py

--- src/scraper_utils.py
+++ src/scraper_utils.py
@@ -1,1 +1,11 @@
+# src/scraper_utils.py
+
+def construct_url(rank_range, date_week):
+    base_url = "https://www.atptour.com/en/rankings/singles?RankRange={rank_range}&Region=all&DateWeek={date_week}"
+    return base_url.format(rank_range=rank_range, date_week=date_week)
+
+def parse_player_data(soup):
+    # Implement parsing logic here
+    # Return a list of dictionaries, each representing a player's data
+    pass
 
